530c4f9a3800caf45934f150d2b6372fbf77a383
Lewington-pitsos <lewingtonpitsos@gmail.com>
2022-12-23 14:29:38 +1100

    add most of the functionality to a new file

--- lib/main.dart
+++ lib/main.dart
@@ -5,82 +5,12 @@ import 'package:permission_handler/permission_handler.dart';
 import 'package:contacts_service/contacts_service.dart';
 import 'package:fluttercontactpicker/fluttercontactpicker.dart';
 import 'dart:async';
-import 'dart:async' show Future;
-import 'dart:convert' show json;
-import 'package:flutter/services.dart' show rootBundle;
-
 import 'package:flutter/widgets.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:workmanager/workmanager.dart';
 import 'package:openai_gpt3_api/openai_gpt3_api.dart';
-
-class Secret {
-  final String SecretKey;
-  Secret({this.SecretKey = ""});
-  factory Secret.fromJson(Map<String, dynamic> jsonMap) {
-    return new Secret(SecretKey: jsonMap["SecretKey"]);
-  }
-
-  @override
-  String toString() {
-    return 'Secret{id: $SecretKey}';
-  }
-}
-
-class SecretLoader {
-  final String secretPath;
-
-  SecretLoader({required this.secretPath});
-  Future<Secret> load() {
-    return rootBundle.loadStructuredData<Secret>(this.secretPath,
-        (jsonStr) async {
-      final secret = Secret.fromJson(json.decode(jsonStr));
-      return secret;
-    });
-  }
-}
-
-class Dog {
-  final int id;
-  final String name;
-  final int age;
-
-  const Dog({
-    required this.id,
-    required this.name,
-    required this.age,
-  });
-
-  Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'name': name,
-      'age': age,
-    };
-  }
-
-  // Implement toString to make it easier to see information about
-  // each dog when using the print statement.
-  @override
-  String toString() {
-    return 'Dog{id: $id, name: $name, age: $age}';
-  }
-}
-
-void executeTask() {
-  Workmanager().executeTask((taskName, inputData) async {
-    print("this is the task $taskName");
-    print("this is the inputData $inputData");
-
-    for (var i = 0; i < 30; i++) {
-      await Future.delayed(Duration(seconds: 1));
-      print("performing $taskName");
-    }
-
-    return Future.value(true);
-  });
-}
+import 'package:pesta/utils.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
